7987760e63e7129994a912f40cb91efccd205459
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-08 16:56:38 -0500

    get input from players

---
+++ main.py
@@ -0,0 +1,7 @@
+player_one = input("Please enter Rock, Paper, or Scissors: ")
+while player_one != "Rock" or "Paper" or "Scissors":
+    player_one = input("Please enter Rock, Paper, or Scissors: ")
+
+player_two = input("Please enter Rock, Paper, or Scissors: ")
+while player_two != "Rock" or "Paper" or "Scissors":
+    player_two = input("Please enter Rock, Paper, or Scissors: ")
