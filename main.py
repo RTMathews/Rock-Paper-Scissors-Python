155403cb0e7c6b97a6dd283b790b2795e217e072
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-08 17:02:27 -0500

    added comparison

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ while player_one != "Rock" or "Paper" or "Scissors":
 player_two = input("Please enter Rock, Paper, or Scissors: ")
 while player_two != "Rock" or "Paper" or "Scissors":
     player_two = input("Please enter Rock, Paper, or Scissors: ")
+
+if player_one == "Rock" and player_two == "Scissors":
+    print("Player 1 Wins!")
+elif player_one == "Paper" and player_two == "Rock":
+    print("Player 1 Wins!")
+elif player_one == "Scissors" and player_two == "Paper":
+    print("player 1 Wins!")
+elif player_one == "Rock" and player_two == "Paper":
+    print("Player 2 Wins!")
+elif player_one == "Paper" and player_two == "Scissors":
+    print("Player 2 Wins!")
+elif player_one == "Scissors" and player_two == "Rock":
+    print("Player 2 Wins!")
+else:
+    print("It's a Draw!")
