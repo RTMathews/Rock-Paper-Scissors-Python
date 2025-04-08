f86cb0aed9e8de6f29c49e6f021b3e7a0667e9b7
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-08 17:46:46 -0500

    add rps function

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-player_one = input("Player 1, Please enter Rock, Paper, or Scissors: ")
-while player_one != "Rock" and player_one != "Paper" and player_one != "Scissors":
+def rps(player_one, player_two):
+ 
     player_one = input("Player 1, Please enter Rock, Paper, or Scissors: ")
+    while player_one != "Rock" and player_one != "Paper" and player_one != "Scissors":
+        player_one = input("Player 1, Please enter Rock, Paper, or Scissors: ")
 
-player_two = input("Player 2, Please enter Rock, Paper, or Scissors: ")
-while player_two != "Rock" and player_two != "Paper" and player_two != "Scissors":
     player_two = input("Player 2, Please enter Rock, Paper, or Scissors: ")
+    while player_two != "Rock" and player_two != "Paper" and player_two != "Scissors":
+        player_two = input("Player 2, Please enter Rock, Paper, or Scissors: ")
 
-if player_one == "Rock" and player_two == "Scissors":
-    print("Rock Beats Scissors! Player 1 Wins!")
-elif player_one == "Paper" and player_two == "Rock":
-    print("Paper Beats Rock! Player 1 Wins!")
-elif player_one == "Scissors" and player_two == "Paper":
-    print("Scissors Beat Paper! Player 1 Wins!")
-elif player_one == "Rock" and player_two == "Paper":
-    print("Paper Beats Rock! Player 2 Wins!")
-elif player_one == "Paper" and player_two == "Scissors":
-    print("Scissors Beats Paper! Player 2 Wins!")
-elif player_one == "Scissors" and player_two == "Rock":
-    print("Rock Beats Scissors! Player 2 Wins!")
-else:
-    print("It's a Draw!")
+    if player_one == "Rock" and player_two == "Scissors":
+        print("Rock Beats Scissors! Player 1 Wins!")
+    elif player_one == "Paper" and player_two == "Rock":
+        print("Paper Beats Rock! Player 1 Wins!")
+    elif player_one == "Scissors" and player_two == "Paper":
+        print("Scissors Beat Paper! Player 1 Wins!")
+    elif player_one == "Rock" and player_two == "Paper":
+        print("Paper Beats Rock! Player 2 Wins!")
+    elif player_one == "Paper" and player_two == "Scissors":
+        print("Scissors Beats Paper! Player 2 Wins!")
+    elif player_one == "Scissors" and player_two == "Rock":
+        print("Rock Beats Scissors! Player 2 Wins!")
+    else:
+        print("It's a Draw!")
